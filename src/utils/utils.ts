332239ee5a541ae6af6df4eb2562e63cbfb73458
adrian <adrianusyoga26@gmail.com>
2025-08-03 17:48:36 +0700

    feat: add utils auth loader

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,15 @@
 import { twMerge } from "tailwind-merge";
 import { type ClassValue, clsx } from "clsx";
+import { useAuthStore } from "@/stores/auth";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const authLoader = () => {
+  const token = useAuthStore.getState().token;
+  if (!token) {
+    throw new Response("Unauthorized", { status: 401 });
+  }
+  useAuthStore.getState().getAuthMe();
+};
