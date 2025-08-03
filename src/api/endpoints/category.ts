7dfa153faa750f7af8fc1229d92de0c253cd0e84
adrian <adrianusyoga26@gmail.com>
2025-08-03 17:24:12 +0700

    feat(api): add api category

--- src/api/endpoints/category.ts
+++ src/api/endpoints/category.ts
@@ -1,11 +1,53 @@
-import type { ApiResponse, WithMeta } from "@/types/common";
+import type {
+  ApiResponse,
+  ApiResponseDelete,
+  BaseParam,
+  WithMeta,
+} from "@/types/common";
 import apiClient from "../client";
 import type { Category } from "@/types/category";
 
-export const apiGetListCategory = (params: unknown) => {
+export const apiCreateCategory = (data: {
+  name: string;
+  code: string;
+  active?: boolean;
+}) => {
+  return apiClient<ApiResponse<Category>>({
+    method: "POST",
+    url: "/api/v1/category",
+    data,
+  });
+};
+
+export const apiGetListCategory = (params: BaseParam<Category>) => {
   return apiClient<ApiResponse<WithMeta<Category[]>>>({
     method: "GET",
     url: "/api/v1/category",
     params,
+  });
+};
+
+export const apiGetDetailCategory = (id: string) => {
+  return apiClient<ApiResponse<Category>>({
+    method: "GET",
+    url: `/api/v1/category/${id}`,
+  });
+};
+
+export const apiUpdateCategory = (
+  id: string,
+  data: { name: string; code: string; active?: boolean }
+) => {
+  return apiClient<ApiResponse<Category>>({
+    method: "PUT",
+    url: `/api/v1/category/${id}`,
+    data,
+  });
+};
+
+export const apiDeleteCategory = (id: string) => {
+  return apiClient<ApiResponseDelete>({
+    method: "DELETE",
+    url: `/api/v1/category/${id}`,
   });
 };
