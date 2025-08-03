d60d3120168efa6482f863c771a93f6810420132
adrian <adrianusyoga26@gmail.com>
2025-08-03 17:46:59 +0700

    feat: adjust root layout

--- src/layout/root-layout.tsx
+++ src/layout/root-layout.tsx
@@ -1,6 +1,8 @@
 import { LayoutNavbar } from "@/components/layout/navbar";
 import { LayoutSidebar } from "@/components/layout/sidebar";
+import { useScreen } from "@/hooks/useScreen";
 import { useAuthStore } from "@/stores/auth";
+import { useUIStore } from "@/stores/ui";
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
@@ -13,12 +15,30 @@ function RootLayout() {
       navigate("/login");
     }
   }, [user, token]);
+
+  const isMobile = useScreen("(max-width: 640px)");
+  const { showSidebar, setShow } = useUIStore();
+
+  useEffect(() => {
+    if (isMobile) {
+      setShow(false);
+    } else {
+      setShow(true);
+    }
+  }, [isMobile]);
+
   return (
     <>
       <LayoutNavbar />
       <LayoutSidebar />
-      <main className="ml-60 p-6 bg-gray-100/50 relative mt-16">
-        <Outlet />
+      <main
+        className={`p-6 mt-16 transition-all duration-300 ${
+          isMobile ? "ml-0" : showSidebar ? "ml-60" : "ml-0"
+        }`}
+      >
+        <div className="relative">
+          <Outlet />
+        </div>
       </main>
     </>
   );
