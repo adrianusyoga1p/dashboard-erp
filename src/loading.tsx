ffb3182854bc525bc3c643ff062f16dc4d6dad20
adrian <adrianusyoga26@gmail.com>
2025-08-03 17:21:30 +0700

    feat: add loading

---
+++ src/loading.tsx
@@ -0,0 +1,7 @@
+export const BaseLoading = () => {
+  return (
+    <div className="fixed flex items-center justify-center inset-0 w-screen h-screen bg-white/40 backdrop-blur-xs z-20">
+      <h1 className="text-xl font-medium">Loading...</h1>
+    </div>
+  );
+};
