f81a824b7d549f27af5d6cf24587731d412400ae
adrian <adrianusyoga26@gmail.com>
2025-08-03 17:24:03 +0700

    feat(api): add api get me

--- src/api/endpoints/auth.ts
+++ src/api/endpoints/auth.ts
@@ -1,11 +1,19 @@
 import type { ApiResponse } from "@/types/common";
 import apiClient from "../client";
 import type { AuthLogin } from "@/types/auth";
+import type { User } from "@/types/user";
 
 export const apiAuthLogin = (data: { email: string; password: string }) => {
   return apiClient<ApiResponse<AuthLogin>>({
     method: "POST",
     url: "api/v1/auth/login",
     data,
+  });
+};
+
+export const apiAuthGetMe = () => {
+  return apiClient<ApiResponse<User>>({
+    method: "GET",
+    url: "/api/v1/auth/me",
   });
 };
