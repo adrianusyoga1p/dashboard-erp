52b1f3947f6bd4dbc2cc40106542b33d3f41fa51
adrian <adrianusyoga26@gmail.com>
2025-08-25 10:58:33 +0700

    feat: add api stock sales, return, out

--- src/api/endpoints/stock.ts
+++ src/api/endpoints/stock.ts
@@ -1,19 +1,70 @@
 import type { ApiResponse, BaseParam, WithMeta } from "@/types/common";
 import apiClient from "../client";
-import type { StockIn, StockInPayload } from "@/types/stock";
+import type { Stocks, StocksPayload, StockSales } from "@/types/stock";
 
-export const apiCreateStockIn = (data: StockInPayload) => {
-  return apiClient<ApiResponse<StockIn>>({
+export const apiCreateStockIn = (data: StocksPayload) => {
+  return apiClient<ApiResponse<Stocks>>({
     method: "POST",
     url: "/api/v1/stock/in",
     data,
   });
 };
 
-export const apiGetListStockIn = (params?: BaseParam<StockIn>) => {
-  return apiClient<ApiResponse<WithMeta<StockIn[]>>>({
+export const apiGetListStockIn = (params?: BaseParam<Stocks>) => {
+  return apiClient<ApiResponse<WithMeta<Stocks[]>>>({
     method: "GET",
     url: "/api/v1/stock/in",
     params,
+  });
+};
+
+export const apiCreateStockOut = (
+  data: Omit<StocksPayload, "minimumStock">
+) => {
+  return apiClient<ApiResponse<Stocks>>({
+    method: "POST",
+    url: "/api/v1/stock/out",
+    data,
+  });
+};
+
+export const apiGetListStockOut = (params: BaseParam<Stocks>) => {
+  return apiClient<ApiResponse<WithMeta<Stocks[]>>>({
+    method: "GET",
+    url: "/api/v1/stock/out",
+    params,
+  });
+};
+
+export const apiCreateStockReturn = (
+  data: Omit<StocksPayload, "minimumStock">
+) => {
+  return apiClient<ApiResponse<Stocks>>({
+    method: "POST",
+    url: "/api/v1/stock/return",
+    data,
+  });
+};
+
+export const apiGetListStockReturn = (params: BaseParam<Stocks>) => {
+  return apiClient<ApiResponse<WithMeta<Stocks[]>>>({
+    method: "GET",
+    url: "/api/v1/stock/return",
+    params,
+  });
+};
+
+export const apiGetListStockSales = (params?: BaseParam<StockSales>) => {
+  return apiClient<ApiResponse<WithMeta<StockSales[]>>>({
+    method: "GET",
+    url: "/api/v1/stock/sales",
+    params,
+  });
+};
+
+export const apiGetDetailStockSales = (salesId: string) => {
+  return apiClient<ApiResponse<StockSales>>({
+    method: "GET",
+    url: "/api/v1/stock/sales/" + salesId,
   });
 };
