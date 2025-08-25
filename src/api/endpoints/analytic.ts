20429fe3c03d9b221809ed2779aea2474d5d293c
adrian <adrianusyoga26@gmail.com>
2025-08-25 10:46:27 +0700

    feat: add api analytic

--- src/api/endpoints/analytic.ts
+++ src/api/endpoints/analytic.ts
@@ -1,24 +1,54 @@
-import type { ApiResponse } from "@/types/common"
-import apiClient from "../client"
-import type { AnalyticStockIn, AnalyticStockOut, AnalyticStockReturn } from "@/types/analytic"
+import type { ApiResponse } from "@/types/common";
+import apiClient from "../client";
+import type {
+  AnalyticDailyRevenue,
+  AnalyticStockIn,
+  AnalyticStockOut,
+  AnalyticStockReturn,
+  AnalyticTopProduct,
+} from "@/types/analytic";
 
 export const apiAnalyticStockIn = () => {
   return apiClient<ApiResponse<AnalyticStockIn>>({
-    method: 'GET',
-    url: 'api/v1/analytic/stock-in'
-  })
-}
+    method: "GET",
+    url: "api/v1/analytic/stock-in",
+  });
+};
 
 export const apiAnalyticStockOut = () => {
   return apiClient<ApiResponse<AnalyticStockOut>>({
-    method: 'GET',
-    url: 'api/v1/analytic/stock-out'
-  })
-}
+    method: "GET",
+    url: "api/v1/analytic/stock-out",
+  });
+};
 
 export const apiAnalyticStockReturn = () => {
   return apiClient<ApiResponse<AnalyticStockReturn>>({
-    method: 'GET',
-    url: 'api/v1/analytic/stock-return'
-  })
-}
+    method: "GET",
+    url: "api/v1/analytic/stock-return",
+  });
+};
+
+export const apiAnalyticTopProducts = (params?: {
+  purchasedAt: string | null;
+}) => {
+  return apiClient<ApiResponse<AnalyticTopProduct[]>>({
+    method: "GET",
+    url: "/api/v1/analytic/top-products",
+    params,
+  });
+};
+
+export const apiAnalyticDailyOrders = () => {
+  return apiClient<ApiResponse<{ date: string; total: string }[]>>({
+    method: "GET",
+    url: "/api/v1/analytic/daily-orders",
+  });
+};
+
+export const apiAnalyticDailyRevenue = () => {
+  return apiClient<ApiResponse<AnalyticDailyRevenue[]>>({
+    method: "GET",
+    url: "/api/v1/analytic/daily-revenue",
+  });
+};
