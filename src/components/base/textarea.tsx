c67553d0358ce4446af441a18d32087c3680dfc1
adrian <adrianusyoga26@gmail.com>
2025-08-28 21:54:03 +0700

    feat: adjust base textarea

--- src/components/base/textarea.tsx
+++ src/components/base/textarea.tsx
@@ -5,11 +5,12 @@ interface BaseTextareaProps
   label?: string;
 }
 
-export const BaseTextarea = ({ label, ...props }: BaseTextareaProps) => {
+export const BaseTextarea = ({ label, value, ...props }: BaseTextareaProps) => {
   return (
     <div>
       <label className="font-semibold text-sm block mb-2">{label}</label>
       <textarea
+        value={value ?? ""}
         className="p-3 w-full border resize-none min-h-28 border-black/30 rounded-lg focus:outline-black transition-colors duration-300"
         {...props}
       />
